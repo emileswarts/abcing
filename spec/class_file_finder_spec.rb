6d4d96bc159ea9b88f2550612a488ad477aabe35
Emile <emile.swarts123@gmail.com>
2015-02-15 19:59:07 +0000

    Test for only scanning rb files

--- spec/class_file_finder_spec.rb
+++ spec/class_file_finder_spec.rb
@@ -4,13 +4,6 @@ require 'abcing/class_file_finder'
 describe ABCing::ClassFileFinder do
   before(:each) do
     Dir.mkdir 'dummy'
-  end
-
-  after(:each) do
-    FileUtils.rm_rf 'dummy'
-  end
-
-  it 'Finds files with class defined in them' do
     out_file = File.new("dummy/foo.rb", "w")
     out_file.puts("class Foo; end;")
     out_file.close
@@ -18,13 +11,37 @@ describe ABCing::ClassFileFinder do
     out_file = File.new("dummy/bar.rb", "w")
     out_file.puts("class Foo; end;")
     out_file.close
+  end
 
-    expected_results = [
-      'dummy/bar.rb',
-      'dummy/foo.rb',
-    ]
+  after(:each) do
+    FileUtils.rm_rf 'dummy'
+  end
 
-    finder = ABCing::ClassFileFinder.new(['dummy'])
-    expect(finder.find).to eq(expected_results)
+  context 'Included files' do
+    it 'Finds files with class defined in them' do
+      expected_results = [
+        'dummy/bar.rb',
+        'dummy/foo.rb',
+      ]
+
+      finder = ABCing::ClassFileFinder.new(['dummy'])
+      expect(finder.find).to eq(expected_results)
+    end
+  end
+
+  context 'Excluded files' do
+    it 'that do not have a .rb extension' do
+      out_file = File.new("dummy/foo_config.txt", "w")
+      out_file.puts("class FooConfig; end;")
+      out_file.close
+
+      expected_results = [
+        'dummy/bar.rb',
+        'dummy/foo.rb',
+      ]
+
+      finder = ABCing::ClassFileFinder.new(['dummy'])
+      expect(finder.find).to eq(expected_results)
+    end
   end
 end
