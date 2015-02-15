cd697e19f08344158b2707263bce6a6985737121
Emile <emile.swarts123@gmail.com>
2015-02-15 21:24:05 +0000

    Create foo fake directories for manual testing

--- lib/abcing/renderer.rb
+++ lib/abcing/renderer.rb
@@ -1,10 +1,19 @@
 module ABCing
   class Renderer
     def initialize
+      @working_copy = ['foo/lib']
+      @test_directories = ['foo/spec']
     end
 
     def render
       alphabet = ('a'..'z').to_a
+
+      working_copy_files = ABCing::ClassFileFinder.new(@working_copy).find
+      test_files = ABCing::ClassFileFinder.new(@test_directories).find
+
+      working_copy_letter_matches = ABCing::AlphabetMatch.new(working_copy_files).letters
+      test_letter_matches = ABCing::AlphabetMatch.new(test_files).letters
+
       p alphabet.join(' ')
     end
   end
