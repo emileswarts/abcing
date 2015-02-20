1997eab689efb868c7f07518d28faa39415a54f7
Emile <emile.swarts123@gmail.com>
2015-02-20 19:34:44 +0000

    It can mark a letter as red if it is not tested

--- spec/colour_alphabet_result_spec.rb
+++ spec/colour_alphabet_result_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/abcing/colour_alphabet_result.rb'
 
 describe ABCing::ColourAlphabetResult do
-  it 'prepares a colourful result' do
+  it 'finds tested class letters' do
     params = {
       test_letters: ['A', 'B'],
       app_letters: ['A', 'B']
@@ -10,13 +10,14 @@ describe ABCing::ColourAlphabetResult do
     result = ABCing::ColourAlphabetResult.new(params).calculate
     expect(result).to include(A: :green, B: :green)
   end
-  it 'prepares a colourful result' do
+
+  it 'does not find a test for a class letter' do
     params = {
-      test_letters: ['A', 'B'],
+      test_letters: ['A'],
       app_letters: ['A', 'B']
     }
 
     result = ABCing::ColourAlphabetResult.new(params).calculate
-    expect(result).to include(A: :green, B: :green)
+    expect(result).to include(A: :green, B: :red)
   end
 end
