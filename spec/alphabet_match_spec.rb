14bdb125e9ef7d4d758ae11fe34aa92f072395c3
Emile <emile.swarts123@gmail.com>
2015-02-23 22:13:58 +0000

    Refactor AlphabetMatch spec.

--- spec/alphabet_match_spec.rb
+++ spec/alphabet_match_spec.rb
@@ -2,34 +2,18 @@
 require_relative '../lib/abcing/alphabet_match'
 
 describe ABCing::AlphabetMatch do
-  before(:each) do
-    Dir.mkdir 'dummy'
-  end
-
-  after(:each) do
-    FileUtils.rm_rf 'dummy'
-  end
-
   it 'collects 2 letters' do
-    class_names = ['Bar', 'Foo']
-    matcher = ABCing::AlphabetMatch.new(class_names)
-
+    matcher = ABCing::AlphabetMatch.new ['Bar', 'Foo']
     expect(matcher.letters).to eq(['B', 'F'])
   end
 
   it 'Only collects unique letters' do
-    class_names = ['Zoo', 'Zebra']
-
-    matcher = ABCing::AlphabetMatch.new(class_names)
-
+    matcher = ABCing::AlphabetMatch.new(['Zoo', 'Zebra'])
     expect(matcher.letters).to eq(['Z'])
   end
 
   it 'Orders letter results alphabetically' do
-    class_names = ['Cobra', 'Acid', 'Bee']
-
-    matcher = ABCing::AlphabetMatch.new(class_names)
-
+    matcher = ABCing::AlphabetMatch.new(['Cobra', 'Acid', 'Bee'])
     expect(matcher.letters).to eq(['A', 'B', 'C'])
   end
 end
