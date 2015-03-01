aa4c24a962a06eafe9f1c15d11f5b9997816da52
Emile <emile.swarts123@gmail.com>
2015-03-01 12:20:30 +0000

    Setup basic benchmark test

---
+++ benchmark/abcing_benchmark.rb
@@ -0,0 +1,32 @@
+require 'benchmark'
+require 'fileutils'
+require_relative '../lib/abcing.rb'
+require_relative './abcing_many_files'
+
+class ABCingBenchmark
+  def self.test_many_files
+    setup
+    ABCing::Benchmark.setup_many_files
+    puts Benchmark.measure { ABCing::Runner.new('benchmark/dummy').run }
+    teardown
+  end
+
+  private
+
+  def self.setup
+    FileUtils.rm_rf 'benchmark/dummy'
+    Dir.mkdir 'benchmark/dummy'
+    Dir.mkdir 'benchmark/dummy/lib'
+    Dir.mkdir 'benchmark/dummy/spec'
+  end
+
+  def self.teardown
+    FileUtils.rm_rf 'benchmark/dummy'
+  end
+end
+
+p "Testing many files"
+
+ABCingBenchmark.setup
+ABCingBenchmark.test_many_files
+ABCingBenchmark.teardown
---
+++ benchmark/abcing_many_files.rb
@@ -0,0 +1,21 @@
+module ABCing
+  class Benchmark
+    def self.setup_many_files
+      200.times do |num|
+        ["foo_#{num}", "bar_#{num}"].each do |name|
+          out_file = File.new("benchmark/dummy/lib/#{ name }.rb", 'w')
+          out_file.puts("class #{ name.capitalize }; end;")
+          out_file.close
+        end
+      end
+
+      200.times do |num|
+        ["foo_#{num}", "bar_#{num}"].each do |name|
+          out_file = File.new("benchmark/dummy/spec/#{ name }.rb", 'w')
+          out_file.puts("#{ name.capitalize }")
+          out_file.close
+        end
+      end
+    end
+  end
+end
