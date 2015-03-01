0073a4d2e0b6b8e6c0227ad7297e1d085153c33b
Emile <emile.swarts123@gmail.com>
2015-03-01 11:24:45 +0000

    Test for missing params to scanner

--- spec/scanner_spec.rb
+++ spec/scanner_spec.rb
@@ -1,14 +1,25 @@
 require_relative '../lib/abcing/scanner.rb'
 
 describe ABCing::Scanner do
+  let(:empty_params) do
+    { app_directories: [], test_directories: [] }
+  end
+
   it 'scans' do
-    params = { app_directories: [], test_directories: [] }
-    scanner = ABCing::Scanner.new(params)
+    scanner = ABCing::Scanner.new(empty_params)
   end
 
   context 'Raises an error' do
     it 'when supplied with nil params' do
       expect { ABCing::Scanner.new(nil) }.to raise_error()
+    end
+
+    it 'when supplied incomplete data' do
+      empty_params.delete(:app_directories)
+      expect { ABCing::Scanner.new(empty_params) }.to raise_error()
+
+      empty_params.delete(:test_directories)
+      expect { ABCing::Scanner.new(empty_params) }.to raise_error()
     end
   end
 end
