1fb4f9c0940b6ce3f0e98b4cae2bf6330e279960
Emile <emile.swarts123@gmail.com>
2015-03-17 18:42:04 +0000

    Method extraction refactoring

--- lib/abcing/class_name_finder.rb
+++ lib/abcing/class_name_finder.rb
@@ -5,7 +5,17 @@ module ABCing
     end
 
     def find
-      @contents.collect { |c| c.scan(/class (\w+).*$/).flatten.uniq }.flatten.uniq
+      class_name_lines.flatten.uniq
+    end
+
+    private
+
+    def class_name_lines
+      @contents.collect { |c| c.scan(class_match_regex).flatten.uniq }
+    end
+
+    def class_match_regex
+      /class (\w+).*$/
     end
   end
 end
